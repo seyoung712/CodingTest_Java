2b49c998397b2e31c0b6088854d1105f7b18def4
ParkSeyoung <84224709+seyoung712@users.noreply.github.com>
2024-01-11 09:27:26 +0900

    [Bronze V] Title: 구구단, Time: 156 ms, Memory: 16156 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        br.close();
+        
+        for(int i=1; i<10; i++){
+            System.out.println(a + " * " + i + " = " + (a*i));
+        }
+    }
+}
